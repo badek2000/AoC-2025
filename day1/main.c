283b7100e768a18135c485dd259f8acd5a11e657
Bartosz <strzelecki100@gmail.com>
2025-12-11 00:18:45 +0100

    [day-1] Use parseFile function

--- day1/main.c
+++ day1/main.c
@@ -4,13 +4,12 @@
 #include <stdlib.h>
 #include <stdlib.h>
 
+#include "file_parser.h"
+
 #define DIAL_UNIQUE_POS ((int)100)
 #define DIAL_STARTING_VAL ((int) 50)
 #define DIAL_LEFT_C 'L'
 #define DIAL_RIGHT_C 'R'
-
-static int current_val = DIAL_STARTING_VAL;
-static size_t total_marked = 0;
 
 typedef enum {
     DIAL_NONE = 0,
@@ -18,41 +17,50 @@ typedef enum {
     DIAL_LEFT = 2,
 } rotate_dial_e;
 
-static void rotateDial(rotate_dial_e dir, int val) {
-    int prev = current_val;
+typedef struct _ctx_s {
+    int current_val;
+    size_t total_marked;
+} ctx_t;
+
+static void rotateDial(rotate_dial_e dir, int val, ctx_t* ctx) {
+    int prev = ctx->current_val;
 
     if (dir == DIAL_NONE) {
         return;    
     }
     
-    total_marked += val / DIAL_UNIQUE_POS;
+    ctx->total_marked += val / DIAL_UNIQUE_POS;
     val %= DIAL_UNIQUE_POS;
     
     if (val == 0) return;
 
     if (dir == DIAL_LEFT) {
-        current_val -= val;
+        ctx->current_val -= val;
     } else if (dir == DIAL_RIGHT) {
-        current_val += val;
+        ctx->current_val += val;
     } else {
         printf("Error undefined rotation dir!\n");
         return;
     }
 
-    if (current_val == 0) {
-        ++total_marked;
-    } else if (current_val < 0) {
-        if (prev != 0) ++total_marked;
-        current_val += DIAL_UNIQUE_POS;
-    } else if (current_val >= DIAL_UNIQUE_POS) {
-        ++total_marked;
-        current_val -= DIAL_UNIQUE_POS;
+    if (ctx->current_val == 0) {
+        ++ctx->total_marked;
+    } else if (ctx->current_val < 0) {
+        if (prev != 0) ++ctx->total_marked;
+        ctx->current_val += DIAL_UNIQUE_POS;
+    } else if (ctx->current_val >= DIAL_UNIQUE_POS) {
+        ++ctx->total_marked;
+        ctx->current_val -= DIAL_UNIQUE_POS;
     }
 }
 
-void parseLine(char* line, size_t len) {
+void parseLine(char* line, size_t len, void* ctx) {
     char* endptr;
     int num;
+
+    if (ctx == NULL) {
+        exit(EXIT_FAILURE);
+    }
 
     rotate_dial_e op = DIAL_NONE;
     
@@ -67,25 +75,19 @@ void parseLine(char* line, size_t len) {
     }
 
     num = strtod(&line[1], &endptr);
-    rotateDial(op, num);
+    rotateDial(op, num, (ctx_t*)ctx);
 }
 
 int main(int argc, char** argv) {
-    FILE* input = fopen(argv[1], "r");
-    size_t len = 0;
-    ssize_t read;
-    char* line = NULL;
+    (void)argc;
 
-    if (!input) {
-        printf("[MAIN] ERROR: Could not open file\n");
-        return 1;
-    }
+    ctx_t ctx = {
+        .current_val = DIAL_STARTING_VAL,
+        .total_marked = 0,
+    };
 
-    while ((read = getline(&line, &len, input)) != -1) {
-        parseLine(line, len);
-    }
-
-    printf("Answer: %lu\n", total_marked);
+    AoC_parseFile(argv[1], parseLine, &ctx);
+    printf("Answer: %lu\n", ctx.total_marked);
 
     return 0;
 }
